ec2df5cb3a7e18425572eab4a41f50091210314b
wtvar <wtvariwnt@gmail.com>
2024-11-14 16:48:42 +0000

    Update main.py
    added rich

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
 from dotenv import load_dotenv
+from rich import print
+from rich import traceback
 
 # Load the .env file
 load_dotenv()
+
+# load the rich stuff
+traceback.install()
 
 # Access the variables
 secret_key = os.getenv('SECRET_KEY')
